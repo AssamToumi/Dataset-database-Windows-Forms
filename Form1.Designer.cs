ff5c6627998d2d6ed6881c09be2f30f0e2573db6
Assam Toumi <assamtomi@outlook.com>
2020-07-03 20:08:38 +0100

    done!

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Dataset
+{
+    partial class btnDelete
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textNumber = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textTelephone = new System.Windows.Forms.TextBox();
+            this.textAddress = new System.Windows.Forms.TextBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btndetetebtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(180, 74);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(608, 261);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textNumber
+            // 
+            this.textNumber.Location = new System.Drawing.Point(67, 74);
+            this.textNumber.Name = "textNumber";
+            this.textNumber.Size = new System.Drawing.Size(100, 20);
+            this.textNumber.TabIndex = 1;
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(67, 100);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(100, 20);
+            this.textName.TabIndex = 1;
+            // 
+            // textTelephone
+            // 
+            this.textTelephone.Location = new System.Drawing.Point(67, 126);
+            this.textTelephone.Name = "textTelephone";
+            this.textTelephone.Size = new System.Drawing.Size(100, 20);
+            this.textTelephone.TabIndex = 1;
+            // 
+            // textAddress
+            // 
+            this.textAddress.Location = new System.Drawing.Point(67, 152);
+            this.textAddress.Name = "textAddress";
+            this.textAddress.Size = new System.Drawing.Size(100, 20);
+            this.textAddress.TabIndex = 1;
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(67, 178);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(100, 20);
+            this.textEmail.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "I_Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Full Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telephone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Email";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(23, 219);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(69, 22);
+            this.btnInsert.TabIndex = 3;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(98, 219);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(69, 22);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btndetetebtn
+            // 
+            this.btndetetebtn.Location = new System.Drawing.Point(719, 341);
+            this.btndetetebtn.Name = "btndetetebtn";
+            this.btndetetebtn.Size = new System.Drawing.Size(69, 22);
+            this.btndetetebtn.TabIndex = 3;
+            this.btndetetebtn.Text = "Delete";
+            this.btndetetebtn.UseVisualStyleBackColor = true;
+            this.btndetetebtn.Click += new System.EventHandler(this.btndetetebtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(688, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(592, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Search I_Number";
+            // 
+            // btnDelete
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btndetetebtn);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.textAddress);
+            this.Controls.Add(this.textTelephone);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textNumber);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "btnDelete";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dataset database for example";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textNumber;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.TextBox textTelephone;
+        private System.Windows.Forms.TextBox textAddress;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btndetetebtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dataset
+{
+    public partial class btnDelete : Form
+    {
+        public btnDelete()
+        {
+            InitializeComponent();
+        }
+        DataSet1TableAdapters.InformationTableAdapter person = new DataSet1TableAdapters.InformationTableAdapter();
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = person.List2();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+           
+            person.insert(textNumber.Text, textName.Text, textTelephone.Text, textAddress.Text, textEmail.Text);
+            foreach (Control item in Controls) if (item is TextBox) item.Text = "";
+            dataGridView1.DataSource = person.List2();
+        }
+
+        private void btndetetebtn_Click(object sender, EventArgs e)
+        {
+            person.Delete1(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            foreach (Control item in Controls) if (item is TextBox) item.Text = "";
+            dataGridView1.DataSource = person.List2();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            person.Update1(textName.Text, textTelephone.Text, textAddress.Text, textEmail.Text, textNumber.Text);
+            foreach (Control item in Controls) if (item is TextBox) item.Text = "";
+            dataGridView1.DataSource = person.List2();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = person.Search2('%'+textBox1.Text+'%');
+        }
+    }
+}
